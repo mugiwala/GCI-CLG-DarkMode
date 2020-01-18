1651002a17a04c2fe4894a25ad3cfe9aa72b52dc
mugiwala <43480121+mugiwala@users.noreply.github.com>
2020-01-18 14:08:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$('#userinfo > div').after('<span class="jid-host" onclick="toggleMode()">Toggle Mode</span><span id="logout-divider">|</span>')
+
+var light = "https://media.copyleftgames.org/css/site.css";
+var dark = "http://mugiwala.github.io/copyleftgames-css/dark-site.css"
+var darkModeToggle = 0
+
+function toggleMode(){
+	if(darkModeToggle === 0){
+		$('link[href*="site.css"]').prop("href", dark);
+		darkModeToggle = 1;
+	} else {
+		$('link[href*="site.css"]').prop("href", light);
+		darkModeToggle = 0;	
+	}
+}

aaca346fbc7718a899d927c62c91d944fd1aab7b
mugiwala <43480121+mugiwala@users.noreply.github.com>
2020-01-18 14:13:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $('#userinfo > div').after('<span class="jid-host" onclick="toggleMode()">Toggle Mode</span><span id="logout-divider">|</span>')
 
 var light = "https://media.copyleftgames.org/css/site.css";
-var dark = "http://mugiwala.github.io/GCI-CLG-DarkMode/dark-site.css"
+var dark = "https://mugiwala.github.io/GCI-CLG-DarkMode/dark-site.css"
 var darkModeToggle = 0
 
 function toggleMode(){
